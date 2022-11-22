7a2fcc5519b5430800e8ce5b7ac5b1480f12e15c
Fernanda Oliveira <109109339+fernandaoliveirasantos@users.noreply.github.com>
2022-11-22 08:38:41 -0300

    JavaScript Tipos Primitivos

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = 'Olá, mundo';
+const senha = "senhaSuperSegura456";
+const StringDeNumeros = '3456';
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Fernanda';
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
+
